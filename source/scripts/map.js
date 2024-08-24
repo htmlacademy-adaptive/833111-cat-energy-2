83e5fc6dd7962732a31294b5e4bf7c4fbcd65e32
MariAndreevna <50734371+MariAndreevna@users.noreply.github.com>
2024-08-25 01:33:38 +0300

    Добавляет графику в разметку (#11)
    * Тестируем сборку
    
    * Тестируем сборку
    
    * Добавляет разметку на странице index
    
    * Добавляет разметку на странице index
    
    * Добавляет разметку
    
    * Исправляет верстку по линтерам
    
    * Исправляет ошибки по линтерам
    
    * Исправляет ошибки по линтерам
    
    * Исправляет ошибки по линтерам
    
    * Исправляет ошибки по линтерам
    
    * Добавляет графику в разметку

---
+++ source/scripts/map.js
@@ -0,0 +1,21 @@
+ymaps.ready(init);
+
+function init() {
+  const myMap = new ymaps.Map('map', {
+    center: [59.938631, 30.323037],
+    zoom: 15,
+    controls: ['zoomControl']
+  });
+
+  const myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
+    balloonContent: 'ул. Большая Конюшенная, д. 19/8<br><strong>Санкт-Петербург</strong>'
+  }, {
+
+    iconLayout: 'default#image',
+    iconImageHref: 'images/map-pin@1x.png',
+    iconImageSize: [57, 53],
+    iconImageOffset: [-15, -42]
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
