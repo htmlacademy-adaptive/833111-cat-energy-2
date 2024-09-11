48d0422986b3a9fab9440ffa49675b969019c208
MariAndreevna <50734371+MariAndreevna@users.noreply.github.com>
2024-09-11 03:01:51 +0300

    Добавляет декоративную графику на страницу index (#14)
    * Добавляет декоративную графику на страницу index
    
    * Добавляет декоративную графику
    
    * Добавляет декоративную графику на страницы
    
    * Добавляет декоративную графику

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+
+/* Бургер-меню */
+const navToggle = document.querySelector('.main-header__toggle');
+const navList = document.querySelector('.main-header__nav-list');
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('main-header__toggle--open');
+  navList.classList.toggle('main-header__nav-list--open');
+});
