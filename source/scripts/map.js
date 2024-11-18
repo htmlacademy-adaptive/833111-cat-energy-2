87692fd8586bfbb24aae79f32e57a1e2e33bebfb
MariAndreevna <libra91@bk.ru>
2024-11-18 19:59:36 +0300

    Исправляет замечания наставника

--- source/scripts/map.js
+++ source/scripts/map.js
@@ -9,15 +9,36 @@ function init() {
     controls: ['zoomControl']
   });
 
-  const myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
-    balloonContent: 'ул. Большая Конюшенная, д. 19/8<br><strong>Санкт-Петербург</strong>'
-  }, {
-
+  const placemarkOptions = {
     iconLayout: 'default#image',
     iconImageHref: 'images/map-pin@1x.png',
-    iconImageSize: [57, 53],
+    iconImageSize: [56, 52],
     iconImageOffset: [-15, -42]
-  });
+  };
+
+  const myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
+    balloonContent: 'ул. Большая Конюшенная, д. 19/8<br><strong>Санкт-Петербург</strong>'
+  }, placemarkOptions);
 
   myMap.geoObjects.add(myPlacemark);
+
+  function updatePlacemarkSize() {
+    const windowWidth = window.innerWidth;
+    if (windowWidth >= 768) { // Планшетный размер и выше
+      myPlacemark.options.set({
+        iconImageSize: [113, 106],
+        iconImageOffset: [-56, -90]
+      });
+    } else {
+      myPlacemark.options.set({
+        iconImageSize: [56, 52],
+        iconImageOffset: [-15, -42]
+      });
+    }
+  }
+
+  updatePlacemarkSize();
+
+  window.addEventListener('resize', updatePlacemarkSize);
 }
+
