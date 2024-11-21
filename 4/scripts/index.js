030cdf7e8266d1a7b631f8abacb82e0a023d6d47
MariAndreevna <MariAndreevna@users.noreply.github.com>
2024-11-21 23:28:47 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/833111-cat-energy-2@2e3cd0147874c97640be0fdc7b56c523c11482af 🚀

---
+++ 4/scripts/index.js
@@ -0,0 +1,1 @@
+var e=document.querySelector(".main-header__toggle"),a=document.querySelector(".main-header__nav-list");a.classList.remove("main-header__nav-list--no-js");e.classList.remove("main-header__toggle--no-js");e.addEventListener("click",()=>{e.classList.toggle("main-header__toggle--open"),a.classList.toggle("main-header__nav-list--open")});
