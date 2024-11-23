a8ba6a6e5d575b0dd30f5b7a451b8ffae8247509
MariAndreevna <libra91@bk.ru>
2024-11-23 19:35:17 +0300

    Исправляет ошибки по критериям

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,9 +3,11 @@
 /* Бургер-меню */
 const navToggle = document.querySelector('.main-header__toggle');
 const navList = document.querySelector('.main-header__nav-list');
+const navigation = document.querySelector('.main-header__navigation');
 
 navList.classList.remove('main-header__nav-list--no-js');
 navToggle.classList.remove('main-header__toggle--no-js');
+navigation.classList.remove('main-header__navigation--no-js');
 
 navToggle.addEventListener('click', () => {
   navToggle.classList.toggle('main-header__toggle--open');
